80486f2220bb7b730ba68b3a57b7fbb7d609d1a4
iamarcayna <i.am.arcayna@gmail.com>
2025-02-21 10:46:46 +0800

    add app layout with sidebar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,13 @@
-import { ThemeProvider } from "@/components/theme-provider/ThemeProvider";
+import AppSidebar from "@/components/app-sidebar/AppSidebar";
+import ThemeProvider from "@/components/theme-provider/ThemeProvider";
 import ThemeSwitcher from "@/components/theme-switcher/ThemeSwitcher";
+
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export const metadata = {
   title: "Frontend Specialist Assessment",
@@ -23,9 +31,21 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <main>
-              <div>{children}</div>
-            </main>
+            <SidebarProvider>
+              <AppSidebar />
+              <SidebarInset>
+                <header className="flex justify-between pr-10 md:pr-14 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                  <div className="flex items-center gap-2 px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                  </div>
+                  <ThemeSwitcher />
+                </header>
+                <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                  {children}
+                </main>
+              </SidebarInset>
+            </SidebarProvider>
           </ThemeProvider>
         </body>
       </html>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,16 @@
 import "@/styles/global.css";
 
-export default function App() {
-  return <div>App</div>;
+export default function Dashboard() {
+  return (
+    <>
+      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+        <div className="aspect-video rounded-xl bg-muted/50" />
+      </div>
+      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+        App
+      </div>
+    </>
+  );
 }
