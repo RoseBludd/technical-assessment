f9cc8cbc75d8363ef79762f3b9c5ee6b3fd38169
iamarcayna <i.am.arcayna@gmail.com>
2025-02-21 14:43:31 +0800

    add table implementation

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
                   </div>
                   <ThemeSwitcher />
                 </header>
-                <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <main className="flex flex-1 flex-col gap-4 p-4 pt-0 xl:mx-10">
                   {children}
                 </main>
               </SidebarInset>
