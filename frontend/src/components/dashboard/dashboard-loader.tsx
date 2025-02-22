2f2faf0345f02715559e850c122bd177503ab9ae
Jayson Tolentino <jaysontolentino.ph@gmail.com>
2025-02-22 15:35:31 +0800

    Complete frontend assessment

--- frontend/src/components/dashboard/dashboard-loader.tsx
+++ frontend/src/components/dashboard/dashboard-loader.tsx
@@ -1,7 +1,15 @@
+import { Skeleton } from "@/src/components/ui/skeleton";
+
 export default function DashboardLoader() {
   return (
-    <div>
-      <p>Loading...</p>
+    <div className="w-full flex gap-4 flex-col">
+      <div className="grid gap-4 grid-cols-1 lg:grid-cols-3">
+        <Skeleton className="flex h-[120px]" />
+        <Skeleton className="flex h-[120px]" />
+        <Skeleton className="flex h-[120px]" />
+      </div>
+
+      <Skeleton className="w-full h-[500px]" />
     </div>
   );
 }
